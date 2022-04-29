b70ae5a309aef6a9dc1f79909321d7ea30d20594
Amit Yadav <88823371+Tyrant670@users.noreply.github.com>
2022-04-29 10:35:06 +0530

    Update Band_name.py

--- Band_name.py
+++ Band_name.py
@@ -1,3 +1,7 @@
+
+# It's a very basic just you need to do string concatenation.
+# So that's how we can generate band name 😅
+
 print("Welcome to the Band Name Generator.")
 street = input("What's name of the city you grew up in?\n")
 pet = input("What's your pet's name?\n")
